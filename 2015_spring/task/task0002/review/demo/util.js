9ccfd76c204a5b053c7d77bb49f806f95cc7c6c8
hangfengnice <nicehangfeng@foxmail.com>
2019-06-22 08:45:03 +0800

    task3

--- 2015_spring/task/task0002/review/demo/util.js
+++ 2015_spring/task/task0002/review/demo/util.js
@@ -45,3 +45,41 @@ console.log(abObj.b.b1[0]);
 console.log(tarObj.a);      // 1
 console.log(tarObj.b.b1[0]); 
 
+// 数组去重
+
+function uniqArray(arr){
+  var obj = {}
+  var ret = []
+  var len = arr.length
+  for(var i = 0; i < len; i ++){
+    var key = arr[i]
+    if(!obj[key]){
+      ret.push(arr[i]);
+      obj[key] = true
+    }
+  }
+  return ret
+}
+
+// 去掉头尾空字符串
+
+function simpleTrim(str){
+  return str.trim()
+}
+
+
+//实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn){
+  for(var i = 0; i < arr.length; i ++){
+    fn(arr[i], i)
+  }
+}
+
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+
+function getObjectLength(obj){
+  
+}
+
+
